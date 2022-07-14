143fa2d47513e371d2c41ecab0db304b9e890359
Carlos Marinho <carlos.marinho20@outlook.com>
2022-07-14 13:42:55 -0300

    test: refatora e adiciona novos testes
    Refatora os testes, de forma que agora um único teste verifica se
    as propriedades estão sendo renderizadas conforme a implementação.
    
    Adiciona teste que verifica se ao clicar no link, o usuário é direcionado
    para a página de detalhes do produto corretamente.

--- src/components/ProductCard/test.tsx
+++ src/components/ProductCard/test.tsx
@@ -1,63 +1,67 @@
-import { screen, render, getByText } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { screen, render, fireEvent } from "@testing-library/react";
+import { unstable_HistoryRouter as Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import ProductCard from ".";
 import { getPrice } from "../../common/utils";
 
 describe("ProductCard component | Unit test", () => {
-  const product = {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    description: "Your perfect pack for everyday use and walks in the forest.",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 3.9,
-      count: 120
-    }
-  };
-
   const makeSut = () => {
-    render(
-      <MemoryRouter>
+    const history = createMemoryHistory({
+      initialEntries: ["/"]
+    });
+
+    const product = {
+      id: 1,
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      price: 109.95,
+      description: "Your perfect pack for everyday use and walks in the forest.",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: {
+        rate: 3.9,
+        count: 120
+      }
+    };
+
+    const result = render(
+      <Router history={history}>
         <ProductCard product={product} />
-      </MemoryRouter>
+      </Router>
     );
+
+    return { result, product, history };
   };
 
-  it("Should render product img", () => {
-    const alt = "Image showing product details";
-
-    makeSut();
-
-    const image = screen.getByAltText(alt);
+  it("Should render product props", () => {
+    const { product } = makeSut();
+    const image = screen.getByAltText("Image showing product details");
+    const title = screen.getByRole("heading", { level: 3 });
+    const price = screen.getByRole("heading", { level: 4 });
 
     expect(image).toBeInTheDocument();
-  });
-
-  it("Should render product title", () => {
-    makeSut();
-
-    const title = screen.getByText(product.title);
+    expect(image.getAttribute("src")).toEqual(product.image);
 
     expect(title).toBeInTheDocument();
-  });
-
-  it("Should render formatted product price", () => {
-    makeSut();
-
-    const price = screen.getByText(getPrice(product.price));
 
     expect(price).toBeInTheDocument();
+    expect(price.textContent).toEqual(getPrice(product.price));
   });
 
   it("Should render add to cart button", () => {
-    const text = "Add to cart";
-
     makeSut();
 
-    const button = screen.getByText(text);
+    const button = screen.getByText("Add to cart");
 
     expect(button).toBeInTheDocument();
+  });
+
+  it("Should navigate to product details page", () => {
+    const { product, history } = makeSut();
+
+    const link = screen.getByRole("link");
+
+    fireEvent.click(link);
+
+    expect(history.location.pathname).toEqual(`/product/${product.id}`);
   });
 });
