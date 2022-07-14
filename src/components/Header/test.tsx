f42d5b9ccfd0cb25c7534cfccccc0884539aa66e
Carlos Marinho <carlos.marinho20@outlook.com>
2022-07-14 11:44:34 -0300

    test: adiciona testes para rotas
    Implementa testes que verificam se a rota mudou corretamente ao clicar no
    botão do carrinho e no logo.

--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -1,20 +1,27 @@
-import { screen, render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { screen, render, fireEvent } from "@testing-library/react";
+import { unstable_HistoryRouter as Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import Header from ".";
 
 describe("Header component | Unit test", () => {
-  const makeSut = () => {
-    render(
-      <MemoryRouter>
+  const makeSut = (initialEntry: "/" | "/cart") => {
+    const history = createMemoryHistory({
+      initialEntries: [initialEntry]
+    });
+
+    const result = render(
+      <Router history={history}>
         <Header />
-      </MemoryRouter>
+      </Router>
     );
+
+    return { result, history };
   };
 
   it("Should render app logo", () => {
     const alt = "App logo, containing a dollar sign inside a circle and the words Fake Commerce.";
 
-    makeSut();
+    makeSut("/");
 
     const logo = screen.getByAltText(alt);
 
@@ -24,10 +31,30 @@ describe("Header component | Unit test", () => {
   it("Should render cart button", () => {
     const text = "Cart";
 
-    makeSut();
+    makeSut("/");
 
     const button = screen.getByText(text);
 
     expect(button).toBeInTheDocument();
+  });
+
+  it("Should navigate to homepage on click", () => {
+    const { history } = makeSut("/cart");
+
+    const link = screen.getByRole("link");
+
+    fireEvent.click(link);
+
+    expect(history.location.pathname).toBe("/");
+  });
+
+  it("Should navigate to cart on click", () => {
+    const { history } = makeSut("/");
+
+    const button = screen.getByText("Cart");
+
+    fireEvent.click(button);
+
+    expect(history.location.pathname).toBe("/cart");
   });
 });
